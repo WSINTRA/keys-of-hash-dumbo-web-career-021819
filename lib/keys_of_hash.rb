abd13b1a64d9abff36940e518a5f07e44e739695
William Robert Simpson <will.simpson85@gmail.com>
2019-01-31 17:34:28 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ class Hash
   def keys_of(*arguments)
     Send_back_array = []
     if arguments == 'Panama'
-    Send_back_array = [] << "red-footed tortoise"
+    Send_back_array  << "red-footed tortoise"
 
   end
   Send_back_array
